25e6e4f09cdef1fc2e1a2075365c2754693766d4
Daniel Gutierrez <danielgudu22@gmail.com>
2019-08-31 11:08:47 -0500

    First version of this bot

---
+++ index.js
@@ -0,0 +1,50 @@
+const request = require('request');
+var Twitter = require('twitter');
+var config = require('../config/config');
+
+var T = new Twitter(config);
+
+// Set up your search parameters
+var params = {
+    q: '#nodejs',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+request.get('https://geek-jokes.sameerkumar.website/api', (err, res, body) => {
+    if(err) {
+        console.log(err) 
+        return;
+    }
+    T.post('statuses/update', { status: body }, function(err, data, response) {
+        if(err) 
+            console.log(err)
+        console.log(data)
+    });
+})
+
+T.get('search/tweets', params, function(err, data, response) {
+    if(!err){
+        // Loop through the returned tweets
+        for(let i = 0; i < data.statuses.length; i++){
+            // Get the tweet Id from the returned data
+            let id = { id: data.statuses[i].id_str }
+            // Try to Favorite the selected Tweet
+            T.post('favorites/create', id, function(err, response){
+            // If the favorite fails, log the error message
+                if(err){
+                console.log(err[0].message);
+                }
+                // If the favorite is successful, log the url of the tweet
+                else{
+                    let username = response.user.screen_name;
+                    let tweetId = response.id_str;
+                    console.log('Favorited: ', `https://twitter.com/${username}/status/${tweetId}`)
+                }
+            });
+        }
+    } else {
+        console.log(err);
+    }
+})
